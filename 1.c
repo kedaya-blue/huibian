d39cc90b43c2a722de47c829c42333c9066bcf88
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2020-12-17 11:52:29 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,7 @@
-#include <stddef.h>
+#include <stdio.h>
 
-void lt(const int* src1, size_t step1, const int* src2, size_t step2, unsigned char* dst, size_t step3,
+__attribute__((noinline))
+static void lt(const int* src1, size_t step1, const int* src2, size_t step2, unsigned char* dst, size_t step3,
 			   size_t width, size_t height) {
 	size_t width_step = 4;
 	for (; height--; src1 += step1, src2 += step2, dst += step3) {
@@ -13,4 +14,22 @@ void lt(const int* src1, size_t step1, const int* src2, size_t step2, unsigned c
 			dst[i] = src1[i] < src2[i] ? 255 : 0;
 		}
 	}
+}
+
+int main() {
+	int* src1 = new int[17 * 17];
+	int* src2 = new int[17 * 17];
+	for (int i = 0; i < 289; ++i) {
+		src1[i] = i;
+		src2[i] = (i % 17) * 17 + i / 17;
+	}
+	unsigned char* dst = new unsigned char[17 * 17];
+	lt(src1, 17, src2, 17, dst, 17, 17, 17);
+	for (int i = 0; i < 17; ++i) {
+		for (int j = 0; j < 17; ++j) {
+			printf("%d ", dst[i * 17 + j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
